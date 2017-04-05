02d7d4843882853c7d27d93753c40436c9a80445
jijianfeng <1031397017@qq.com>
2017-04-05 08:45:11 +0800

    zookeeper基础

---
+++ ALibaba/src/com/jjf/zookeeper/Client.java
@@ -0,0 +1,44 @@
+package com.jjf.zookeeper;
+
+import org.apache.zookeeper.*;
+
+import java.io.IOException;
+
+/**
+ * Created by jjf_lenovo on 2017/4/4.
+ */
+public class Client implements Watcher {
+    ZooKeeper zk;
+    String hostPort;
+
+    Client(String hostPort){
+        this.hostPort = hostPort;
+    }
+
+    void startZK() throws IOException {
+        zk = new ZooKeeper(hostPort,15000,this);
+    }
+
+    String queueCommand(String command) throws KeeperException, InterruptedException {
+        while(true){
+            try{
+                String name = zk.create("/tasks/task-",command.getBytes(),
+                        ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                        CreateMode.EPHEMERAL_SEQUENTIAL);
+                return name;
+//                break;
+            }
+            catch (KeeperException.NodeExistsException e){
+                throw new KeeperException.NodeExistsException("already appears to be running");
+            }
+            catch (KeeperException.ConnectionLossException e){
+
+            }
+        }
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+
+    }
+}
