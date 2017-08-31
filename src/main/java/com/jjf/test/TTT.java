939b8f93039a6d4a11d746ab00d37bf78d6117f8
jijianfeng <1031397017@qq.com>
2017-08-31 16:54:12 +0800

    Arrays.addAll

--- src/main/java/com/jjf/test/TTT.java
+++ src/main/java/com/jjf/test/TTT.java
@@ -11,47 +11,10 @@ import lombok.Data;
  * Created by jijianfeng on 2017/7/25.
  */
 public class TTT {
+
   @Test
-  public void testSwitch(){
-    Bean bean = Bean.selectById("ban1231ana");
-    switch (bean){
-      case Apple:{
-        System.out.println(Bean.Apple.getId());
-      }
-      case Banana:{
-        System.out.println(Bean.Banana.getId());
-        break;
-      }
-    }
-  }
-}
+  public void test() {
 
-enum Bean {
-  Apple("apple","Æ»¹û"),
-  Banana("banana","Ïã½¶");
-
-  private String id;
-  private String name;
-
-  Bean(String id, String name) {
-    this.id = id;
-    this.name = name;
   }
 
-  public String getId() {
-    return id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public static Bean selectById(String id){
-    for (Bean bean : Bean.values()){
-      if (bean.getId().equals(id)){
-        return bean;
-      }
-    }
-    return  null;
-  }
 }
