83a4433567ba3b3c644f95961a8d8cbfe8b07d2d
jijianfeng <1031397017@qq.com>
2017-09-05 17:01:32 +0800

    jstream的几个例子

--- src/main/java/com/jjf/lambda/StringMapTest.java
+++ src/main/java/com/jjf/lambda/StringMapTest.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.stream.Collectors;
 
 /**
@@ -27,13 +29,24 @@ public class StringMapTest {
   @Test
   public void test2(){
     int[] ids = {0,1,2,3,4};
-    List list = new ArrayList();
+    List<Integer> list = new ArrayList();
     for(int i=0;i<4;i++){
       Integer temp = new Integer(i);
       Arrays.stream(ids)
           .filter(value->value==temp)
-          .forEach(val->list.add(temp));
+          .forEach(list::add);
     }
     list.forEach(System.out::print);
+  }
+
+  @Test
+  public void test3(){
+    String[] ids = {"1","2","3","4"};
+    List<String> list = new ArrayList();
+    String out = Arrays.stream(ids)
+        .filter(id-> id.equals("5"))
+        .findFirst()
+        .get();//.isPresent();//.orElse("meiyou la ");
+    System.out.println(out);
   }
 }
