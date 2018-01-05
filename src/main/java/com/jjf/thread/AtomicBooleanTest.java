8a79d1d2db7860dace1eca31c0299582631b58f3
jijianfeng <jijianfeng@youzan.com>
2018-01-05 15:43:36 +0800

    remark

---
+++ src/main/java/com/jjf/thread/AtomicBooleanTest.java
@@ -0,0 +1,13 @@
+package com.jjf.thread;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author by jijianfeng on 2017/12/29.
+ */
+public class AtomicBooleanTest {
+    public static void main(String[] args) {
+        AtomicBoolean atomicBoolean = new AtomicBoolean();
+
+    }
+}
